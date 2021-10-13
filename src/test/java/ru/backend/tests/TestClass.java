e3cade54f35000408591ce747e13b8810516a7a2
BorisovMaxim <soulmaxim@gmail.com>
2021-10-13 13:19:41 +0400

    TestClassik

--- src/test/java/ru/backend/tests/TestClass.java
+++ src/test/java/ru/backend/tests/TestClass.java
@@ -3,5 +3,6 @@ package ru.backend.tests;
 public class TestClass {
     public static void main(String[] args) {
         System.out.println("Hello");
+        System.out.println("New word");
     }
 }
