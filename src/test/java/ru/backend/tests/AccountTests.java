2e662e8de9d5ce44d942f9417a9e69f16fce95f0
BorisovMaxim <soulmaxim@gmail.com>
2021-10-13 12:57:49 +0400

    Initial commit

---
+++ src/test/java/ru/backend/tests/AccountTests.java
@@ -0,0 +1,51 @@
+package ru.backend.tests;
+
+import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class AccountTests extends BaseTest {
+    @Test
+    void getAccountInfoTest() {
+        given()
+                .header("Authorization", token)
+                .when()
+                .get("https://api.imgur.com/3/account/{username}", username)
+                .then()
+                .statusCode(200);
+    }
+
+    @Test
+    void getAccountInfoWithLoggingTest() {
+        given()
+                .header("Authorization", token)
+                .log()
+                .method()
+                .log()
+                .uri()
+                .when()
+                .get("https://api.imgur.com/3/account/{username}", username)
+                .prettyPeek()
+                .then()
+                .statusCode(200);
+    }
+
+    @Test
+    void getAccountInfoWithAssertionsInGivenTest() {
+        given()
+                .header("Authorization", token)
+                .log()
+                .method()
+                .log()
+                .uri()
+                .expect()
+                .statusCode(200)
+                .body("data.url", equalTo(username))
+                .body("success", equalTo(true))
+                .body("status", equalTo(200))
+                .contentType("application/json")
+                .when()
+                .get("https://api.imgur.com/3/account/{username}", username)
+                .prettyPeek();
+    }
+}
