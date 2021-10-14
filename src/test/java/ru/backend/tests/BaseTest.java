346e9ee4b0966ee37b201c23fcf1625fee88c845
BorisovMaxim <soulmaxim@gmail.com>
2021-10-14 14:06:26 +0400

    update BaseTest

--- src/test/java/ru/backend/tests/BaseTest.java
+++ src/test/java/ru/backend/tests/BaseTest.java
@@ -2,6 +2,12 @@ package ru.backend.tests;
 
 import io.qameta.allure.restassured.AllureRestAssured;
 import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
 import org.junit.jupiter.api.BeforeAll;
 
 import java.io.FileInputStream;
@@ -9,7 +15,13 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+
 public abstract class BaseTest {
+    static ResponseSpecification positiveResponseSpecification;
+    static RequestSpecification requestSpecificationWithAuth;
+
     static Properties properties = new Properties();
     static String token;
     static String username;
@@ -18,9 +30,23 @@ public abstract class BaseTest {
     static void beforeAll() {
         RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
         RestAssured.filters(new AllureRestAssured());
+        RestAssured.baseURI = "https://api.imgur.com/3";
         getProperties();
         token = properties.getProperty("token");
         username = properties.getProperty("username");
+
+        positiveResponseSpecification = new ResponseSpecBuilder()
+                .expectBody("status", equalTo(200))
+                .expectBody("success", is(true))
+                .expectContentType(ContentType.JSON)
+                .expectStatusCode(200)
+                .build();
+
+        requestSpecificationWithAuth = new RequestSpecBuilder()
+                .addHeader("Authorization", token)
+                .build();
+
+//        RestAssured.responseSpecification = positiveResponseSpecification;
     }
 
     private static void getProperties() {
